0aa1d347fccca092253a9fbe811378db559c1bf8
MOLOVEWON <98653874+MOLOVEWON@users.noreply.github.com>
2022-02-20 23:43:14 +0900

    Add files via upload

--- js/background.js
+++ js/background.js
@@ -4,11 +4,11 @@ const chosenImg = images[Math.floor(Math.random() * images.length)];
 
 //img.src = `img/${chosenImg}`;
 
-//document.body.style.background = `url(img/${chosenImg})`;
+document.body.style.background = `url(img/${chosenImg})`;
 //document.body.style.backgroundSize = "cover";
 //document.body.style.background = "center";
 //document.body.style.background = 'red';
-const bgImage = document.createElement("img");
-bgImage.src = `img/${chosenImg}`;
+//const bgImage = document.createElement("img");
+//bgImage.src = `img/${chosenImg}`;
 
-document.body.appendChild(bgImage);
+//document.body.appendChild(bgImage);
